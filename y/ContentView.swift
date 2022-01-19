44383552caf030a84609a07b7a068aae4cbe8883
Raneem <Raneem1ty@gmail.com>
2022-01-19 15:18:37 +0300

    Initial Commit

---
+++ y/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  y
+//
+//  Created by Raneem Raneem on 16/06/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ y/yApp.swift
@@ -0,0 +1,17 @@
+//
+//  yApp.swift
+//  y
+//
+//  Created by Raneem Raneem on 16/06/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct yApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
